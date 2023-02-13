b822b548722ac979597634a7b1ee447e8ae84296
Tran Trong Hien <tronghientran18@gmail.com>
2023-02-13 09:49:11 +0700

    update end.

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ import SearchIcon from './search.svg';
 // c032e2d7
 const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
 
-// const movie1 = {
-//     "Title": "Italian Spiderman",
-//     "Year": "2007",
-//     "imdbID": "tt2705436",
-//     "Type": "movie",
-//     "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-// }
-
 const App = () => {
     // Set state hook , it like init value
     const [movies, setMovies] = useState([]);
